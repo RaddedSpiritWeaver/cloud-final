3ae38f0b09d66e6c9c409817248941518e84bef7
Erfan Alizadeh Noohi <erfan.noohi@gmail.com>
2021-02-06 22:22:19 +0330

    init 2

---
+++ load-sim/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace load_sim.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
