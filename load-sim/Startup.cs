f2300c32ce45898c56b6119393f8a1b1fd6a16d7
Erfan Alizadeh Noohi <erfan.noohi@gmail.com>
2021-02-07 05:00:56 +0330

    added swagger

--- load-sim/Startup.cs
+++ load-sim/Startup.cs
@@ -26,16 +26,24 @@ namespace load_sim
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+            
             app.UseHttpsRedirection();
 
             app.UseRouting();
