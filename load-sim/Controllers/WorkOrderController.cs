16326d3b097aaf2244abbfeb28e4c14ea1229670
arminafzali <arminafzali23@gmail.com>
2021-02-07 11:46:49 +0330

    add controllers for testing

--- load-sim/Controllers/WorkOrderController.cs
+++ load-sim/Controllers/WorkOrderController.cs
@@ -1,5 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -21,10 +24,55 @@ namespace load_sim.Controllers
         {
             var rng = new Random();
             var l = new List<long>();
-            for(long i = 0; i < 1000_000_000; i++)
+            for (long i = 0; i < 1000_000_000; i++)
                 l.Add(rng.Next());
 
             return l;
         }
+        [HttpGet("{order}/{num}")]
+        public ActionResult test(int order, int num)
+        {
+            return Ok(Order(order, num));
+        }
+        [HttpGet("testMemory/{order}")]
+
+        public ActionResult testMemory(long order)
+        {
+            List<Person> people = new List<Person>();
+            for (long i = 0; i < Math.Pow(10,order); i++)
+            {
+                Person p = new Person()
+                {
+                    FirstName =i%2==0? "armin":"erfan",
+                    LastName = i % 2 == 0 ? "afzali" : "noohi",
+                    Age = i % 2 == 0 ? 23 : 22,
+                    Id = i,
+                };
+                people.Add(p);
+            }
+            return Ok("done");
+
+        }
+        private long Order(int order, int num)
+        {
+            long sum = 0;
+            if (order == 0 || num == 0)
+                return 1;
+            for (int i = 0; i < order; i++)
+            {
+                sum += Order(order, num - 1) + 1;
+            }
+            return sum;
+        }
+
+
     }
+    public class Person{
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+    }
+
 }
